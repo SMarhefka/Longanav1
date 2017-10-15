dd0342309046202ac0c5daef45e03560d0eb3b56
Svetlana Marhefka <Svetlana Marhefka@SMARHEFKA>
2017-10-15 04:36:20 -0400

    computer - documented the computer cpp and h files

--- Longanav1/computer.cpp
+++ Longanav1/computer.cpp
@@ -1,12 +1,36 @@
+/************************************************************************
+* Name:	Svetlana Marhefka												*
+* Project : Project 1 - Longana											*
+* Class : CMPS 366 Organization of Programming Languages (OPL)			*
+* Date : 10/14/2017														*
+*************************************************************************/
+
 #include "stdafx.h"
 #include "computer.h"
 
+/************************************************************************
+Function Name: computer()
+Purpose: Default constructor
+Parameters: None
+Return Value: Constructor
+Local Variables: None
+Algorithm: None
+Assistance Received: None
+************************************************************************/
 // default constructor
 computer::computer()
 {
 }
 
-// default destructor
+/************************************************************************
+Function Name: computer()
+Purpose: Default destructor
+Parameters: None
+Return Value: Constructor
+Local Variables: None
+Algorithm: None
+Assistance Received: None
+************************************************************************/
 computer::~computer()
 {
 }
--- Longanav1/computer.h
+++ Longanav1/computer.h
@@ -1,18 +1,25 @@
+/************************************************************************
+* Name:	Svetlana Marhefka												*
+* Project : Project 1 - Longana											*
+* Class : CMPS 366 Organization of Programming Languages (OPL)			*
+* Date : 10/14/2017														*
+*************************************************************************/
+
 #pragma once
 #include "player.h"
 
 class computer : public player
 {
 public:
-	// default constructor
+	// Default constructor
 	computer();
 
+	// Overloaded constructor
 	computer(playerHand a_inUserHand, int a_inScore, bool a_inComPassed, bool a_inPlyerPassed) : player(a_inUserHand, a_inScore, a_inComPassed, a_inPlyerPassed)
 	{
 		m_playerName = "Computer";
 	};
-	// default destructor
-	~computer(); 
 
-private:
+	// Default destructor
+	~computer(); 
 };
